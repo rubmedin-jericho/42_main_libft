5b919f997f4e13445d3be0255b28f694e3ac2aaf
rubmedin-jericho <rubenmedinamartinez2016@gmail.com>
2024-11-11 19:06:51 +0100

    push

--- auto_42_main.c
+++ auto_42_main.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -19,27 +20,57 @@ int	select_funct(char **s1, char *s2)
 int	main(int argc, char **argv)
 {
 	char *basic_functions[24] = {"ft_memset",\
-	"ft_bzero","ft_memcpy","ft_memccpy",\
-	"ft_memmove","ft_memchr","ft_memcmp",\
-	"ft_strlen","ft_isalpha","ft_isdigit",\
-	"ft_isalnum","ft_isascii","ft_isprint",\
-	"ft_toupper","ft_tolower","ft_strchr",\
-	"ft_strrchr","ft_strncmp","ft_strlcpy",\
-	"ft_strlcat","ft_strnstr","ft_atoi",\
-	"ft_calloc","ft_strdup"};
+	"ft_bzero",\
+	"ft_memcpy",\
+	"ft_memccpy",\
+	"ft_memmove",\
+	"ft_memchr",\
+	"ft_memcmp",\
+	"ft_strlen",\
+	"ft_isalpha",\
+	"ft_isdigit",\
+	"ft_isalnum",\
+	"ft_isascii",\
+	"ft_isprint",\
+	"ft_toupper",\
+	"ft_tolower",\
+	"ft_strchr",\
+	"ft_strrchr",\
+	"ft_strncmp",\
+	"ft_strlcpy",\
+	"ft_strlcat",\
+	"ft_strnstr",\
+	"ft_atoi",\
+	"ft_calloc",\
+	"ft_strdup"};
 
-	char  mk_target[50] = "make TARGET=";	
-	char  mk_clean[50] = "make clean TARGET=";
-	
+	char	mk_target[50] = "make TARGET=";	
+	char	tmp[50];
+	char	mk_clean[50] = "make clean TARGET=";
+	int	i = 0;
+
 	if(argc == 1)
 	{
-		printf("ERROR_1\n");
-		return (0);
+		while(i < 24)
+		{
+			memset(tmp, 0, sizeof(tmp));
+			strcpy(tmp, mk_target);
+			strcat(tmp, basic_functions[i]);
+			system(tmp);
+			i++;
+		}
 	}
 	else if(argc == 2 && (strcmp(argv[1], "clean") == 0))
 	{
-		printf("ERROR_3\n");
-		return (0);	
+		while(i < 24)		
+		{
+			memset(tmp, 0, sizeof(tmp));
+			strcpy(tmp, mk_clean);
+			strcat(tmp, basic_functions[i]);
+			system(tmp);
+			memset(tmp, 0, 50);
+			i++;
+		}
 	}
 	else if(argc == 2 && (select_funct(basic_functions, argv[1]) != 0))
 	{
