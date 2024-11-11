09ec8b36c6523085283fd7c79791fa75e1a3680d
rmedina- <ruskymartinez@gmail.com>
2024-11-11 01:19:09 +0100

    push

---
+++ main_ft_bzero.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_ft_bzero.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubmedin <rubmedin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 23:52:18 by rubmedin          #+#    #+#             */
+/*   Updated: 2024/11/11 00:42:46 by rubmedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+int     main(int argc, char **argv)                                
+{       
+        char *str;
+        char *str_org;                                             
+        if(argc != 3)                                              
+        {       
+                printf("Error <few/to many> parameters");          
+                return (0);                                        
+        }
+        str = argv[1];
+        str_org = argv[1];
+        printf("before_str_user: %s\n",str);
+        printf("before_str_original: %s\n",str_org);               
+        ft_bzero(str, atoi(argv[2]));
+        bzero(str_org, atoi(argv[2]));
+        printf("------------------\n \after_str_user: %s\n",str + atoi(argv[2]));
+        printf("after_str_original: %s\n",str_org+ atoi(argv[2])); 
+        return (0);                                                
+} 
--- makefile
+++ makefile
@@ -1,22 +1,23 @@
 CC = gcc
 FLAGS = -Wall -Werror -Wextra
-SOURCE = ../ft_memset.c
 TARGET ?= $(error "TARGET no especificado. Usa: make TARGET=<nombre>")
+SOURCE = $(OUT_DIR)$(TARGET).c
 MAIN = main_$(TARGET).c 
-OBJECTS = $(SOURCE:.c=.o) $(MAIN:.c=.o)
+OBJECTS = $(TARGET).o
 OUT_DIR = ../
 HEADER = ../libft.h 
 
 .PHONY: all clean
 
-all: $(OUT_DIR)/$(TARGET)
-	
-%.o: %.c $(HEADER)
+all: $(OUT_DIR)$(TARGET)
+
+$(OUT_DIR)$(TARGET): $(OBJECTS) $(MAIN)
+	$(CC) $(FLAGS) $(MAIN) $(OBJECTS) -o $(OUT_DIR)$(TARGET)
+
+
+$(TARGET).o: $(SOURCE) $(HEADER)
 	$(CC) $(FLAGS) -c $< -o $@
 
-$(OUT_DIR)/$(TARGET): $(OBJECTS)
-	$(CC) $(FLAGS) -o $@ $^
-
 clean:
-	rm -rf $(OUT_DIR)/$(TARGET)
-	rm -rf $(OBJECTS)
+	rm -rf $(OUT_DIR)$(TARGET)
+	rm -rf $(OBJECTS) 
